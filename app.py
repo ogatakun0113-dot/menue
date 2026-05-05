4347fc6a9c5fea9d07a4c25c75c99767c00ac97b
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-05 11:23:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ st.markdown("---")
 # 先頭や途中に全角スペースが入らないよう修正済みです
 apps = {
     "🔢 伝送換算アシスト(200-999)": "https://200-999-o244vkvrcu7wufxndezrc5.streamlit.app/",
-     "🔢 伝送換算アシスト(990-FD0)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
-    "📡 無線設備計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
+     "📡 無線設備計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
+    "🔢 伝送換算アシスト(990-FD0)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
     "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",
     "📡 W ⇄dBm 相互変換": "https://dxzv2ve2tarf3epbjekvk6.streamlit.app/",
      "⚡ 消費電力計算": "https://p-e-i-w-ciq65gzsje57g5t2bjibev.streamlit.app/",
@@ -30,6 +30,7 @@ apps = {
     "⚖️ 質量・重量計算": "https://cx6vhj5b5eaum7yrtghzkm.streamlit.app/",
     "🌐 IPv4サブネット": "https://lan-ip-6a84chs983d8qrywcfi8ek.streamlit.app/",
     "🎨 カラー抵抗計算": "https://lbocbpi8ohzxyhudcamols.streamlit.app/",
+    
 }
 
 # 2列でボタンを配置
