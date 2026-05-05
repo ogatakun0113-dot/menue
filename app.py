d1575d1687ecb8280ee603c986c5ebab1665142f
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-05 09:37:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ apps = {
      "🔢 伝送換算アシスト(990-FD0)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
     "📡 無線設備計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
     "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",
-    "📡 dBm ⇄ W 相互変換": "https://yltvyhyz4sgt94sdxrwumm.streamlit.app/",
+    "📡 W ⇄dBm 相互変換": "https://dxzv2ve2tarf3epbjekvk6.streamlit.app/",
      "⚡ 消費電力計算": "https://p-e-i-w-ciq65gzsje57g5t2bjibev.streamlit.app/",
     "📏 電圧降下計算": "https://denatus-kouka-mkzokbttfxvxryb4x9kxe6.streamlit.app/",
     "📐 面積・坪・畳 換算": "https://m9ebpan3hnocsbchjgnc3c.streamlit.app/",
