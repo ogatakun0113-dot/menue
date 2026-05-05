c6cb6481a663b4c8affacea779cdfed819b02c58
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-05 11:32:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("---")
 # 先頭や途中に全角スペースが入らないよう修正済みです
 apps = {
     "🔢 伝送換算アシスト(200-999)": "https://200-999-o244vkvrcu7wufxndezrc5.streamlit.app/",
-     "📡 無線設備計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
+     "📡 ＶＳＷＲ計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
     "🔢 伝送換算アシスト(990-FD0)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
     "⚡ 消費電力計算": "https://p-e-i-w-ciq65gzsje57g5t2bjibev.streamlit.app/",
     "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",
