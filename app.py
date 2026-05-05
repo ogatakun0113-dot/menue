fdd6f3fcdc0bd3332e2991aa6917c34c040d9853
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-05 09:02:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ st.markdown("---")
 # アプリのリストとURL
 # 先頭や途中に全角スペースが入らないよう修正済みです
 apps = {
-    "🔢 伝送換算アシスト": "https://200-999-o244vkvrcu7wufxndezrc5.streamlit.app/",
-    "⚡ 消費電力計算": "https://dbm-w-calc-ogata.streamlit.app/",
-    "📡 dBm⇄dBμV変換": "https://dbm-dbuv-conv-ogata.streamlit.app/",
-    "📏 電圧降下計算": "https://voltage-drop-ogata.streamlit.app/",
+    "🔢 伝送換算アシスト(200-999)": "https://200-999-o244vkvrcu7wufxndezrc5.streamlit.app/",
+     "🔢 伝送換算アシスト(990-FD0)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
+    "⚡ 消費電力計算": "https://p-e-i-w-ciq65gzsje57g5t2bjibev.streamlit.app/",
+    "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",
+    "📏 電圧降下計算": "https://denatus-kouka-mkzokbttfxvxryb4x9kxe6.streamlit.app/",
     "⚖️ 質量・重量計算": "https://weight-calc-ogata.streamlit.app/",
     "🌐 IPv4サブネット": "https://ip-calc-ogata.streamlit.app/"
 }
