c2931affbe8ed533b8132e3ca0e6100e0e8c15ae
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-06 18:57:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ apps = {
     "📐 面積・坪・畳 換算": "https://m9ebpan3hnocsbchjgnc3c.streamlit.app/",
       
     "⚖️ 質量・重量計算": "https://cx6vhj5b5eaum7yrtghzkm.streamlit.app/",
-   "📡 W ⇄ dBμV 相互変換": "https://w-db-v-5oemhzumvdhiaf4shewlhp.streamlit.app/",
+   "📡 W ⇄ dBm 相互変換": "https://dxzv2ve2tarf3epbjekvk6.streamlit.app/",
     "🎨 カラー抵抗計算": "https://lbocbpi8ohzxyhudcamols.streamlit.app/",
     
 }
