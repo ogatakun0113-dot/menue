e2892d351c27fd9bcb6b0670b92f8e650c6e1c65
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-06 19:06:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ st.markdown("---")
 apps = {
     "🔢 伝送値換算(200-999)(DEC)": "https://200-999-o244vkvrcu7wufxndezrc5.streamlit.app/",
     "🔢 伝送値換算(990-FD0)(HEX)": "https://990-fd0-br5qdwlqpcg6xgzndxrfxk.streamlit.app/",
+       "📡 ＶＳＷＲ計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
     "🔢 伝送値換算(3200-FA00)(HEX)": "https://denatus-kouka-aktoharzrfntzg3ujbemx3.streamlit.app/",
-   "📡 ＶＳＷＲ計算": "https://y2gpx3glcpxabyvquqwsu8.streamlit.app/",
     "⚡ 消費電力計算": "https://p-e-i-w-ciq65gzsje57g5t2bjibev.streamlit.app/",
     "🌐 IPv4サブネット": "https://lan-ip-6a84chs983d8qrywcfi8ek.streamlit.app/", 
      "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",   
