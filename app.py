426a8d74a3252da2b31226aa74e097338f1bf014
ogatakun0113-dot <ogatakun0113@gmail.com>
2026-05-06 18:53:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,11 @@ apps = {
      "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",   
     "📏 電圧降下計算": "https://denatus-kouka-mkzokbttfxvxryb4x9kxe6.streamlit.app/",
     "📡 dBm⇄dBμV変換": "https://dbm-dbuv-converter-aeqcnqxhxypoemfnmbecwh.streamlit.app/",
+    "📡 W ⇄ dBμV 相互変換": "https://w-db-v-5oemhzumvdhiaf4shewlhp.streamlit.app/",
     "📐 面積・坪・畳 換算": "https://m9ebpan3hnocsbchjgnc3c.streamlit.app/",
-      "📡 W ⇄ dBμV 相互変換": "https://w-db-v-5oemhzumvdhiaf4shewlhp.streamlit.app/",
+      
     "⚖️ 質量・重量計算": "https://cx6vhj5b5eaum7yrtghzkm.streamlit.app/",
-  
+   "📡 W ⇄ dBμV 相互変換": "https://w-db-v-5oemhzumvdhiaf4shewlhp.streamlit.app/",
     "🎨 カラー抵抗計算": "https://lbocbpi8ohzxyhudcamols.streamlit.app/",
     
 }
